9d638b3755d930439a850b0ee689a081365ee481
Farhaanaz <farhaanaz069@gmail.com>
2026-04-17 19:58:55 +0800

    update security group policy

--- terraform/securityGroup.tf
+++ terraform/securityGroup.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "bukabuku-sg" {
+resource "aws_security_group" "bukabuku_sg" {
   name        = "bukabuku-sg"
   description = "bukabuku-sg"
 
@@ -8,21 +8,37 @@ resource "aws_security_group" "bukabuku-sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "sshFromMyIP" {
-  security_group_id = aws_security_group.bukabuku-sg.id
+  security_group_id = aws_security_group.bukabuku_sg.id
   cidr_ipv4         = "103.94.5.235/32" # get it from whatsmyip and /32 means only that IP address
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.bukabuku_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 80
+  ip_protocol = "tcp"
+  to_port = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.bukabuku_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  ip_protocol = "tcp"
+  to_port = 443
+}
+
 resource "aws_vpc_security_group_egress_rule" "allowAllOutboundIPv4" {
-  security_group_id = aws_security_group.bukabuku-sg.id
+  security_group_id = aws_security_group.bukabuku_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allowAllOutboundIPv6" {
-  security_group_id = aws_security_group.bukabuku-sg.id
+  security_group_id = aws_security_group.bukabuku_sg.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1"
 }
