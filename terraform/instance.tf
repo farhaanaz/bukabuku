7556ee1f776e3780cb6cb7b732e1f00459e0df03
Farhaanaz <farhaanaz069@gmail.com>
2026-04-17 20:23:45 +0800

    update terraform variables name

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bukabukuwebinstance" {
   ami                    = data.aws_ami.amiID.id
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.bukabuku.key_name
+  key_name               = aws_key_pair.bukabuku_key.key_name
   vpc_security_group_ids = [aws_security_group.bukabuku_sg.id]
   availability_zone      = "us-east-1a"
 
@@ -15,7 +15,7 @@ resource "aws_instance" "bukabukuwebinstance" {
 
 }
 
-resource "aws_ec2_instance_state" "web-state" {
+resource "aws_ec2_instance_state" "web_state" {
   instance_id = aws_instance.bukabukuwebinstance.id
   state       = "running"
 
--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "bukabuku" {
+resource "aws_key_pair" "bukabuku_key" {
   key_name   = "bukabuku-key"
   public_key = file("${path.module}/bukabuku-key.pub")
 }
