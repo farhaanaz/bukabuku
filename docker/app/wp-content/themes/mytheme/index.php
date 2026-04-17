5cd7fc34f29bbfb0b07c985895933ff6bedbea90
Farhaanaz <farhaanaz069@gmail.com>
2026-04-17 23:20:33 +0800

    fix index.php link

--- docker/app/wp-content/themes/mytheme/index.php
+++ docker/app/wp-content/themes/mytheme/index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/index-D6Hm_bob.css">    
+
 <div id="root"></div>
 
 <script type="module" src="<?php echo get_template_directory_uri(); ?>/assets/index-D6Hm_bob.js"></script>
