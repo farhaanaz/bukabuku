3d3d49dc0a83b63176308e3c1a1378dd218ab5ce
Farhaanaz <farhaanaz069@gmail.com>
2026-04-18 20:35:53 +0800

    reconfigure user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+set -e
+
 apt update -y
-apt install docker.io docker-compose git -y
+apt install -y ca-certificates curl gnupg lsb-release git
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) \
+  signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo $VERSION_CODENAME) stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 systemctl enable docker
 systemctl start docker
 
@@ -9,6 +26,9 @@ usermod -aG docker ubuntu
 
 cd /home/ubuntu
 git clone https://github.com/farhaanaz/bukabuku.git
-cd bukabuku/docker
-docker-compose up -d
 
+sleep 10
+
+cd bukabuku/docker
+
+docker compose up -d
