fed87f0d4e4ef447ce0012a4a44772b4f91847e3
Farhaanaz <farhaanaz069@gmail.com>
2026-04-24 01:31:02 +0800

    fix path of new template

--- docker/app/wp-content/themes/mytheme/index.php
+++ docker/app/wp-content/themes/mytheme/index.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
-</head>
-<body>
+<?php get_header(); ?>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/assets/index-CYGfNM1H.css">
 
 <div id="root"></div>
 
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/index-CYGfNM1H.css">
+<script type="module" src="<?php echo get_template_directory_uri(); ?>/dist/assets/index-CUMpmRZb.js"></script>
 
-<script type="module" src="<?php echo get_template_directory_uri(); ?>/assets/index-CUMpmRZb.js"></script>
-
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
