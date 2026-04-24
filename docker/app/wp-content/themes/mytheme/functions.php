493ea20e24e8324c41bb492bd07d22f567617c4c
Farhaanaz <farhaanaz069@gmail.com>
2026-04-25 03:54:16 +0800

    fix: vite manifest to root dist

--- docker/app/wp-content/themes/mytheme/functions.php
+++ docker/app/wp-content/themes/mytheme/functions.php
@@ -21,44 +21,20 @@ function get_vite_asset($entry, $type = 'js') {
     return $manifest[$entry]['file'];
 
 function mytheme_enqueue_assets() {
-    $manifest_path = get_template_directory() . '/dist/manifest.json';
+    wp_enqueue_style(
+        'mytheme-css',
+        get_template_directory_uri() . '/dist/assets/index-CYGfNM1H.css',
+        [],
+        null
+    );
 
-    if (!file_exists($manifest_path)) {
-        error_log('Manifest not found');
-        return;
-    }
-
-    $manifest = json_decode(file_get_contents($manifest_path), true);
-
-    if (!isset($manifest['index.html'])) {
-        error_log('Entry index.html not found in manifest');
-        return;
-    }
-
-    $entry = $manifest['index.html'];
-
-    // CSS
-    if (!empty($entry['css'])) {
-        foreach ($entry['css'] as $css_file) {
-            wp_enqueue_style(
-                'mytheme-css',
-                get_template_directory_uri() . '/dist/' . $css_file,
-                [],
-                null
-            );
-        }
-    }
-
-    // JS
-    if (!empty($entry['file'])) {
-        wp_enqueue_script(
-            'mytheme-js',
-            get_template_directory_uri() . '/dist/' . $entry['file'],
-            [],
-            null,
-            true
-        );
-    }
+    wp_enqueue_script(
+        'mytheme-js',
+        get_template_directory_uri() . '/dist/assets/index-DgKzrG_l.js',
+        [],
+        null,
+        true
+    );
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_assets');
 }
