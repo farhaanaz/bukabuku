60984c8aa2641c3aec570acb46d38f6e5166fc2c
Farhaanaz <farhaanaz069@gmail.com>
2026-04-25 01:51:54 +0800

    reconfigure wordpress when instance deployed

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -50,6 +50,12 @@ sudo docker compose up -d --remove-orphans
 
 sleep 10
 
+sudo docker exec bukabuku-wordpress bash -c "
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&
+chmod +x wp-cli.phar &&
+mv wp-cli.phar /usr/local/bin/wp
+"
+
 # FIX PERMISSION
 sudo chown -R ubuntu:www-data /home/ubuntu/bukabuku/docker/app/wp-content/themes
 sudo chmod -R 775 /home/ubuntu/bukabuku/docker/app/wp-content/themes
@@ -58,5 +64,32 @@ sudo chmod -R 775 /home/ubuntu/bukabuku/docker/app/wp-content/themes
 sudo chown -R ubuntu:www-data /home/ubuntu/bukabuku/docker/app/wp-content/themes/mytheme
 sudo chmod -R 775 /home/ubuntu/bukabuku/docker/app/wp-content/themes/mytheme
 
-# setgid biar file baru ikut group
+echo "Waiting for WordPress container..."
+
+until sudo docker exec bukabuku-wordpress wp core is-installed --allow-root 2>/dev/null; do
+  echo "WordPress not ready yet..."
+  sleep 5
+  break
+done
+
+# ==============================
+# AUTO INSTALL WORDPRESS (ONLY IF NOT INSTALLED)
+# ==============================
+if ! sudo docker exec bukabuku-wordpress wp core is-installed --allow-root; then
+  echo "Installing WordPress..."
+
+  PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)
+
+  sudo docker exec bukabuku-wordpress wp core install \
+    --url="http://$PUBLIC_IP" \
+    --title="Bukabuku" \
+    --admin_user="admin" \
+    --admin_password="admin123" \
+    --admin_email="admin@mail.com" \
+    --skip-email \
+    --allow-root
+else
+  echo "WordPress already installed, skipping..."
+fi
+
 sudo find /home/ubuntu/bukabuku/docker/app/wp-content/themes -type d -exec chmod g+s {} \;
