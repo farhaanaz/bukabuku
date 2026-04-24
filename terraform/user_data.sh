bee415e31bb6bfee0ac3ea24b24dc13d6880d890
Farhaanaz <farhaanaz069@gmail.com>
2026-04-25 02:27:27 +0800

    reconfig user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -4,7 +4,6 @@ set -e
 
 exec > /var/log/user-data.log 2>&1
 
-
 # INSTALL BASIC PACKAGES
 apt update -y
 apt install -y ca-certificates curl gnupg lsb-release git
@@ -31,7 +30,6 @@ systemctl start docker
 usermod -aG docker ubuntu
 
 # CLONE / UPDATE REPO
-
 cd /home/ubuntu
 
 if [ ! -d "bukabuku" ]; then
@@ -44,36 +42,42 @@ git reset --hard origin/main
 
 sleep 20
 
-
 # DOCKER COMPOSE
-
 cd /home/ubuntu/bukabuku/docker
 
-# reset container + volume 
 sudo docker compose down -v || true
-
-# pull latest image
 sudo docker compose pull
-
-# start container
 sudo docker compose up -d --remove-orphans
 
-
 # WAIT CONTAINER READY
-
 echo "Waiting containers..."
-sleep 15
+sleep 20
 
-# WAIT DATABASE READY
+# WAIT MYSQL READY
+echo "Waiting MySQL..."
 
-echo "Waiting database..."
-
-until sudo docker exec bukabuku-wpcli wp db check --path=/var/www/html --allow-root >/dev/null 2>&1; do
-  echo "Database not ready..."
+until sudo docker exec mysql mysqladmin ping -h "localhost" --silent; do
+  echo "MySQL not ready..."
   sleep 5
 done
 
-echo "Database ready!"
+echo "MySQL is ready!"
+
+# WAIT NETWORK STABILIZE
+echo "Waiting Docker network..."
+sleep 15
+
+# CHECK DB CONNECTION VIA WPCLI
+echo "Waiting WordPress DB connection..."
+
+until sudo docker exec bukabuku-wpcli bash -c "
+wp db check --path=/var/www/html --allow-root
+" >/dev/null 2>&1; do
+  echo "WordPress DB not ready..."
+  sleep 5
+done
+
+echo "WordPress DB ready!"
 
 # AUTO INSTALL WORDPRESS
 
@@ -96,9 +100,12 @@ if ! sudo docker exec bukabuku-wpcli wp core is-installed --path=/var/www/html -
 else
   echo "WordPress already installed"
 fi
-# FIX PERMISSION (CRITICAL)
 
+
+# FIX PERMISSION
 sudo chown -R ubuntu:www-data /home/ubuntu/bukabuku/docker/app/wp-content/themes
 sudo chmod -R 775 /home/ubuntu/bukabuku/docker/app/wp-content/themes
 
 sudo find /home/ubuntu/bukabuku/docker/app/wp-content/themes -type d -exec chmod g+s {} \;
+
+echo "SETUP COMPLETE 🚀"
