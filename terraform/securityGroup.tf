a2b6e03f75f28d5001d33ccca87e9770bb78b26f
Farhaanaz <farhaanaz069@gmail.com>
2026-04-24 20:21:34 +0800

    open port 3000 in securityGroup.tf for monitoring

--- terraform/securityGroup.tf
+++ terraform/securityGroup.tf
@@ -36,6 +36,14 @@ resource "aws_vpc_security_group_ingress_rule" "https" {
   to_port = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "grafana" {
+  security_group_id = aws_security_group.bukabuku_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 3000
+  ip_protocol = "tcp"
+  to_port = 3000
+}
+
 resource "aws_vpc_security_group_egress_rule" "allowAllOutboundIPv4" {
   security_group_id = aws_security_group.bukabuku_sg.id
   cidr_ipv4         = "0.0.0.0/0"
