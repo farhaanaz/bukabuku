f36ae81af93976b5b57b8e63019936d0ebcb76bf
Farhaanaz <farhaanaz069@gmail.com>
2026-04-25 04:11:02 +0800

    fix: vite manifest to root dist

--- docker/app/wp-content/themes/mytheme/index.php
+++ docker/app/wp-content/themes/mytheme/index.php
@@ -1,14 +1,9 @@
 <?php
 get_header();
-
-$js = get_vite_asset('index.html');
-$css = get_vite_asset('index.html', 'css');
 ?>
-
-<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/dist/assets/' . $css; ?>">
 
 <div id="root"></div>
 
-<script type="module" src="<?php echo get_template_directory_uri() . '/dist/assets/' . $js; ?>"></script>
-
-<?php get_footer(); ?>
+<?php
+get_footer();
+?>
